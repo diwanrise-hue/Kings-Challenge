166d14eee1e5bf334207e21e3193ac4e6f2328aa
diwanrise-hue <diwanrise@gmail.com>
2026-09-06 02:48:25 +0300

    Update dama-scripts.js

--- dama/dama-scripts.js
+++ dama/dama-scripts.js
@@ -855,8 +855,8 @@ window.renderRankTrack = function() {
     // 🟢 تقليل الفراغات الداخلية ودفع الإطار للأسفل مسافة 15 بيكسل لحماية البطاقات
     const wrapper = container.parentElement;
     if (wrapper) {
-        wrapper.style.setProperty('padding-top', '1px', 'important');
-        wrapper.style.setProperty('padding-bottom', '1px', 'important');
+        wrapper.style.setProperty('padding-top', '4px', 'important');
+        wrapper.style.setProperty('padding-bottom', '4px', 'important');
         wrapper.style.setProperty('transform', 'translateY(15px)', 'important'); 
     }
 
