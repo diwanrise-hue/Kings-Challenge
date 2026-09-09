c7dd759d367880431c3fdb41cf824ea44b71ce7d
diwanrise-hue <diwanrise@gmail.com>
2026-09-09 22:07:13 +0300

    Update uiController.js

--- dama/uiController.js
+++ dama/uiController.js
@@ -1144,7 +1144,7 @@ export const ui = {
         if (typeof gameState.turnTimeLeft === 'undefined' || gameState.turnTimeLeft === null) {
             gameState.turnTimeLeft = 45;
         }
-      },
+      }
   
         const updateTimerDisplay = () => {
             this.setTxt('turn-countdown', `${t('time_left')} ${gameState.turnTimeLeft}s`);
